0c0d2cd1f90392d704873a6ec631a0c10c4d4478
Jiyoon Pyo <jiyoonp0228@gmail.com>
2024-11-21 13:29:03 -0600

    append using OpenAI

---
+++ OpenAI/README.md
@@ -0,0 +1,9 @@
+# Using OpenAI
+
+```
+pip install python-dotenv
+```
+
+
+.env
+OPENAI_API_KEY={your API Key}
---
+++ OpenAI/_inference.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+
+from LLM_Prompting_Backbones.OpenAI.inference import OpenAI
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)   # for exponential backoff
+
+# Load OPENAI API Key
+load_dotenv()
+
+client = OpenAI(
+    api_key = os.getenv('OPENAI_API_KEY')
+)
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def prompt_openai(question: str,
+                  openai_model='gpt-4o-mini') -> str:
+    completion = client.chat.completions.create(
+        model=openai_model,
+        messages=[
+            {'role': 'system', 'content': 'You are a helpful system'},
+            {'role': 'user', 'content': f'{question}'},
+        ]
+    )
+
+    return str(completion.choices[0].message.content)
